17d5a6adca65637337d9d67eca14882d58f45956
Chetas Patel <94209839+MrRogerTag1345@users.noreply.github.com>
2025-06-10 19:45:49 -0400

    Started to Create GUI

--- Address Book JAVA/Address Book/src/Main.java
+++ Address Book JAVA/Address Book/src/Main.java
@@ -1,49 +1,76 @@
 /*
  * Made By Chetas Patel
- */
+*/
 
- import java.util.List;
- import java.util.Scanner;
- 
- public class Main {
-     public static void main(String[] args) {
- 
-         InputIOClass reader = new InputIOClass();
-         String path = new String("Address Book JAVA/Address Book/src/AddressBook.txt");
-         List<String[]> addressBook = reader.Input(path);
-         Process functionProcessor = new Process();
- 
-         Scanner scnObj = new Scanner(System.in);
- 
-         System.out.println("Welcome! \nThis is an Address Book.");
-         System.out.println("1. Add Contact \n2. View Contacts \n3. Search Contact \n4. Delete Contact \n5. Exit");
-         System.out.print("Selection: ");
-         
-         Integer option = null;
-         try {
-             option = Integer.parseInt(scnObj.nextLine());
-         } catch (NumberFormatException e) {
-             System.out.println("Invalid input. Please enter a number between 1 and 5.");
-         }
- 
-         switch (option) {
-             case 1:
-                 functionProcessor.AddNewContact(addressBook);
-                 break;
-             case 2:
-                 functionProcessor.View(addressBook);
-                 break;
-             case 3:
-                 functionProcessor.SearchContact(addressBook);
-                 break;
-             case 4:
-                 functionProcessor.RemoveContact(addressBook);
-             case 5:
-                 System.exit(0);
-                 break;
-             default:
-                 System.exit(0);
-         }
-     }
- }
- 
+import java.awt.*;
+import java.util.List;
+import java.util.Scanner;
+import javax.swing.*;
+
+public class Main {
+
+    private JFrame frame;
+    private JPanel panel;
+
+
+    public void createGUI() {
+        frame = new JFrame();
+        panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 10));
+        panel.setLayout(new GroupLayout(panel));
+        panel.setPreferredSize(new Dimension(800, 600));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Test");
+        frame.pack(); // Calculate layout sizes first
+
+        // Best way to center on any screen
+        frame.setLocationRelativeTo(null); // <-- Centers the frame
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Main gui = new Main();
+
+        gui.createGUI();
+
+        InputIOClass reader = new InputIOClass();
+        String path = new String("Address Book JAVA/Address Book/src/AddressBook.txt");
+        List<String[]> addressBook = reader.Input(path);
+        Process functionProcessor = new Process();
+
+        Scanner scnObj = new Scanner(System.in);
+
+        System.out.println("Welcome! \nThis is an Address Book.");
+        System.out.println("1. Add Contact \n2. View Contacts \n3. Search Contact \n4. Delete Contact \n5. Exit");
+        System.out.print("Selection: ");
+
+        Integer option = null;
+        try {
+            option = Integer.parseInt(scnObj.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a number between 1 and 5.");
+        }
+
+        switch (option) {
+            case 1:
+                functionProcessor.AddNewContact(addressBook);
+                break;
+            case 2:
+                functionProcessor.View(addressBook);
+                break;
+            case 3:
+                functionProcessor.SearchContact(addressBook);
+                break;
+            case 4:
+                functionProcessor.RemoveContact(addressBook);
+            case 5:
+                System.exit(0);
+                break;
+            default:
+                System.exit(0);
+        }
+    }
+}
